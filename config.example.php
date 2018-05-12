fc6b764b342d9d3ad5269bdd677b0d8b366b97a5
sajjadhussain123 <33265360+sajjadhussain123@users.noreply.github.com>
2018-05-12 08:52:53 -0700

    Update config.example.php

--- config.example.php
+++ config.example.php
@@ -1,6 +1,6 @@
 
 <?php
 // see README.md for an explanation of these config params
-$TWILIO_ACCOUNT_SID = 'your account sid here';
-$TWILIO_API_KEY = 'your API key here';
-$TWILIO_API_SECRET = 'your API secret here';
+$TWILIO_ACCOUNT_SID = 'AC145919653f545cad5e81f4389e993eaa';
+$TWILIO_API_KEY = 'SK6902ee3f36b2342a2268088b8a83755e';
+$TWILIO_API_SECRET = 'IwdJipYiAFexkh4BqR5ZoShRIF3HweE2';
